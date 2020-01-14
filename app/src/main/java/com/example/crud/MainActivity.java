fbd566417ae0ecd0a81ac5ab4e6071fc144e0ddd
EverythingAndroidPro <trucktrack0@gmail.com>
2020-01-14 19:06:27 +0200

    first commit

---
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStore, btnGetall;
+    private EditText etname, ethobby;
+    private DatabaseHelper databaseHelper;
+
+    private ListView listView;
+    private ArrayList<UserModel> userModelArrayList;
+    private CustomAdapter customAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+
+        btnStore = (Button) findViewById(R.id.btnstore);
+ //       btnGetall = (Button) findViewById(R.id.btnget);
+        etname = (EditText) findViewById(R.id.etname);
+        ethobby = (EditText) findViewById(R.id.ethobby);
+
+        listView = (ListView) findViewById(R.id.lv);
+
+        databaseHelper = new DatabaseHelper(this);
+
+        userModelArrayList = databaseHelper.getAllUsers();
+
+        customAdapter = new CustomAdapter(this,userModelArrayList);
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,UpdateDeleteActivity.class);
+                intent.putExtra("user",userModelArrayList.get(position));
+                startActivity(intent);
+            }
+        });
+
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHelper.addUserDetail(etname.getText().toString(), ethobby.getText().toString());
+                printDatabase();
+                etname.setText("");
+                ethobby.setText("");
+                Toast.makeText(MainActivity.this, "Stored Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        btnGetall.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,GetAllUsersActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+    public void printDatabase(){
+        listView = (ListView) findViewById(R.id.lv);
+
+        databaseHelper = new DatabaseHelper(this);
+
+        userModelArrayList = databaseHelper.getAllUsers();
+
+        customAdapter = new CustomAdapter(this,userModelArrayList);
+        listView.setAdapter(customAdapter);
+    }
+
+}
